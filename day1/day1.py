c8288944409abf6e8ed1dc4c8afbd2d91e326368
herites <95563388+herites@users.noreply.github.com>
2022-12-02 09:01:27 +0100

    refactor:

--- day1/day1.py
+++ day1/day1.py
@@ -1,31 +1,22 @@
 def main() -> None:
     elf_dict = dict_from_input()
-    most_cals = max(elf_dict.values())
-    print("Biggus dickus: " + str(most_cals))
-    print("Top3: " + str(get_top3(elf_dict)))
+    print(max(elf_dict.values()))
+    print(sum(sorted(elf_dict.values(), reverse=True)[:3]))
 
 
 def dict_from_input() -> dict:
     with open("input.txt") as f:
         lines = f.readlines()
     elf_dict = {}
-    elf_num = 1
+    current_elf = 1
     for i in lines:
-        if i == "\n":
-            elf_num += 1
+        if i == "\n":  # doesn't account for first line being newline
+            current_elf += 1
         else:
-            if elf_num not in elf_dict.keys():
-                elf_dict[elf_num] = 0
-            elf_dict[elf_num] += int(i)
+            if current_elf not in elf_dict.keys():
+                elf_dict[current_elf] = 0
+            elf_dict[current_elf] += int(i)
     return elf_dict
-
-
-def get_top3(elf_dict: dict) -> dict:
-    top3 = {}
-    for i in range(3):
-        top_elf = max(elf_dict, key=elf_dict.get)
-        top3[top_elf] = elf_dict.pop(top_elf)
-    return sum(top3.values())
 
 
 if __name__ == "__main__":
